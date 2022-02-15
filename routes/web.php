9304ef995e0d657e6b93d9a02b73e1a4ad1ea731
Garcia <viniciusgarcia@versatecnologia.com.br>
2022-02-15 06:04:01 -0300

    Adcionado rotas de: pesquisa de bases 'data-base/search', e rotas para controller tools.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ConectionController;
+use App\Http\Controllers\TooslController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,5 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::resource('/', \App\Http\Controllers\PrincipalController::class);
 Route::resource('/data-base', \App\Http\Controllers\DataBaseController::class);
+Route::resource('/user-data-base', \App\Http\Controllers\UserDataBaseController::class);
 
+Route::get('/data-base/search', [ConectionController::class, 'testConection'])->name('data-base.search');
 Route::get('/conection', [ConectionController::class, 'testConection'])->name('test.conection');
+Route::get('/tools/compare-data-base', [TooslController::class, 'compareDataBase'])->name('tools.compare-data-base');
+Route::get('/tools/integridade-campo', [TooslController::class, 'integridadeCampo'])->name('tools.integridade-campo');
+Route::get('/tools/load-data-base', [TooslController::class, 'loadDataBase'])->name('tools.load-data-base');
+Route::post('/tools/load-data-base-load', [TooslController::class, 'loadDataBaseLoad'])->name('tools.load-data-base-load');
+Route::post('/tools/load-data-base', [TooslController::class, 'loadDataBaseSave'])->name('tools.load-data-base-save');
